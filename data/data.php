db0d6df02c19ef781f9e986c6fbbe0bdcb0e1fe0
tverjus <thibaut.verjus@gmail.com>
2016-05-09 10:54:26 -0400

    ajout d'un tableau de données

---
+++ data/data.php
@@ -0,0 +1,38 @@
+<?php
+
+$data = array(
+    0 => array(
+        "forfait" => "Punta Cana",
+        "categorie" => "tout_inclus",
+        "continent" => "Amérique du Nord",
+        "Pays" => "Mexique",
+        "description" => "Punta Cana, le soleil d'or, l'eau fraîche et les couleurs du Mexique. Tout pour s'amuser, se détendre... ou les deux!!!",
+        "duree_jours" => 10,
+        "prix" => 1400,
+        "inclus" => "Inclus: navette de l'aéroport, hôtel, resto (deux repas), plage, piscine, buffet, bar, nombreux spectacles et activités locales.",
+        "photo" => "mexique.jpg",
+    ),
+    1 => array(
+        "forfait" => "Au Coeur de Banff",
+        "categorie" => "marcheurs",
+        "continent" => "Amérique du Nord",
+        "Pays" => "Canada",
+        "description" => "Suivez le guide dans l'Ouest Canadien. Le parc national de Bannf ne cessera de vous étonner: faune, flore, couchers de soleil  féériques. À vos sacs!",
+        "duree_jours" => 12,
+        "prix" => 2200,
+        "inclus" => "Inclus: navette de l'aéroport, transport sur le site, guide expérimenté et bivoic.",
+        "photo" => "vancouver.jpg",
+    ),
+    2 => array(
+        "forfait" => "Charme, Raffinement, Thaïlande",
+        "categorie" => "luxe",
+        "continent" => "Asie",
+        "Pays" => "Thaïlande",
+        "description" => "La Thaïlande à votre rythme. Un cadre étrange et beau, des sites historiques, paysages et une gastronomie délicieusement dépaysants... et notre équipe aux petits soins pour vous.",
+        "duree_jours" => 15,
+        "prix" => 8200,
+        "inclus" => "Inclus: navette de l'aéroport, hôtel, voiture avec ou sans chauffeur, resto (deux repas), plage, piscine, lounge, bar, nombreux spectacles et activités",
+        "photo" => "thailande.jpg",
+    ),
+
+);
